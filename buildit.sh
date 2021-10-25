3e74feef8b82c71dcd9faf6141765a57d4a2b06d
Jim McCann <jimccann@redhat.com>
2021-10-25 17:09:59 -0400

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -3,7 +3,7 @@ RUNIT='MotionPro_Linux_RedHat_x64_build-8.sh'
 if test -f "$RUNIT"; then
    echo "$RUNIT exists."
 else
-   wget --no-check-certificate -N https://support.arraynetworks.net/prx/001/http/supportportal.arraynetworks.net/downloads/pkg_9_4_0_421/MP_Linux_1.2.11/$RUNIT -O $RUNIT 
+   wget --no-check-certificate -N https://support.arraynetworks.net/prx/001/http/supportportal.arraynetworks.net/downloads/pkg_9_4_0_421/MP_Linux_1.2.11/$RUNIT
 fi
 
 sudo podman build -t ibm-cloud-vpn2.0 .
