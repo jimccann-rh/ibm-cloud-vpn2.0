f7e6d76730b9a7e5f01f6e03fb2c4fb9dd7056ce
Jim McCann <jimccann@redhat.com>
2021-10-26 23:43:51 -0400

    Update runit.sh
    give container a static name

--- runit.sh
+++ runit.sh
@@ -4,5 +4,5 @@ read -s password
 source <(grep = vpn.ini)
 printf "\n$method $host $user $log\n"
 
-sudo podman run --rm -it --privileged --network host ibm-cloud-vpn2.0 --method $method --host $host --user $user --passwd $password --loglevel $log 
+sudo podman run --rm -it --name VPNcontainer --privileged --network host ibm-cloud-vpn2.0 --method $method --host $host --user $user --passwd $password --loglevel $log 
 
